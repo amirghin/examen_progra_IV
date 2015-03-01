d367b456b1c9933ab72589bc4c5b9c4a4b4f456f
Adrian <adrian@aastorga.com>
2015-03-01 07:22:26 -0600

    final

--- modificar_partido.php
+++ modificar_partido.php
@@ -6,13 +6,18 @@ include_once("partidos.php");
 $partido = new partidos;
 
 
-
-if(isset($_POST["jornada"], $_POST["equipo_local"], $_POST["equipo_visita"])){
+if(isset($_GET["jornada"], $_GET["equipo_local"], $_GET["equipo_visita"])){
 
 	
-	$partido->buscar_partido($conexion, $_POST["jornada"], $_POST["equipo_local"], $_POST["equipo_visita"]);
+	$partido->buscar_partido($conexion, $_GET["jornada"], $_GET["equipo_local"], $_GET["equipo_visita"]);
 	
 
+}
+
+
+if(isset($_POST["goles_local"], $_POST["goles_visita"], $_POST["fecha_partido"], $_POST["hora_partido"])){
+
+	$partido->modificar_partido($conexion, $_POST["goles_local"], $_POST["goles_visita"], $_POST["fecha_partido"], $_POST["hora_partido"]);
 }
 
 ?>
@@ -21,19 +26,29 @@ if(isset($_POST["jornada"], $_POST["equipo_local"], $_POST["equipo_visita"])){
 <html>
 <script>
 
+function buscar_partido(){
+var jornada = document.getElementById("jornada").value;
+var eq_local = document.getElementById("equipo_local").value;
+var eq_visita = document.getElementById("equipo_visita").value;
 
+
+parent.location = 'modificar_partido.php?jornada='+ jornada + '&equipo_local=' + eq_local + '&equipo_visita=' + eq_visita;
+   
+} 
 function limpiar_pantalla(){
-   parent.location='insertar_partido.php';
+   parent.location='modificar_partido.php';
 }
 
 </script>
 <body>
+<p id="demo"></p>
+
 <form action="" method="POST">
 	<table style="width:10%" align="left">
 		 <tr>
 		   <td>Jornada:</td>
 		   <td>
-		   		<select name="jornada" required="required" <?php echo $partido->disabled_buscar;?>>
+		   		<select id="jornada" name="jornada" required="required" <?php echo $partido->disabled_buscar;?>>
 		   			<option disabled selected><?php echo $partido->drop_jornada;?></option>
 					<option value="1"> 1 </option>
 					<option value="2"> 2 </option>
@@ -55,8 +70,8 @@ function limpiar_pantalla(){
 		 <tr>
 		   <td>Equipo Local:</td>
 		   <td>
-		   	<select name="equipo_local" required="required" <?php echo $partido->disabled_buscar;?>>
-		   		<option disabled selected><?php echo $partido->drop_local;?></option>
+		   	<select id="equipo_local" name="equipo_local" required="required" <?php echo $partido->disabled_buscar;?>>
+		   		<option disabled selected value=<?php echo $partido->codigo_equipo_local;?>><?php echo $partido->drop_local;?></option>
 				<?php $partido->lista_equipos($conexion);?>
 			</select>
 		   </td> 
@@ -64,8 +79,8 @@ function limpiar_pantalla(){
 		 <tr>
 		  <td>Equipo Visita:</td>
 		  <td>
-		  	<select name="equipo_visita" required="required" <?php echo $partido->disabled_buscar;?>>
-		  		<option disabled selected><?php echo $partido->drop_visita;?></option>
+		  	<select id="equipo_visita" name="equipo_visita" required="required" <?php echo $partido->disabled_buscar;?>>
+		  		<option disabled selected value=<?php echo $partido->codigo_equipo_visita;?>><?php echo $partido->drop_visita;?></option>
 				<?php $partido->lista_equipos($conexion);?>
 			</select>
 		  </td>
@@ -91,7 +106,7 @@ function limpiar_pantalla(){
 		  <td><input type="time" name="hora_partido"  required="required" step="1" min="09:00:00" max="20:00:00"<?php echo $partido->disabled_campos;?> value=<?php echo $partido->hora_partido;?> ></td>
 		 </tr>
 		 <tr>
-		 	<td align="center"><input type="submit" value="Buscar" name="Buscar"></td>
+		 	<td align="center"><input type="button" value="Buscar" name="Buscar" id="Buscar" onclick="buscar_partido();"></td>
 		 	<td align="center"><input type="submit" value="Actualizar" name="Actualizar"></td>
 		 	<td align="center"><input type="button" value="Cancelar" name="Cancelar" id="Cancelar" onclick="limpiar_pantalla();" /></td>
 		 </tr>
